5ca920af9c9350e4050aa10876d8595a307b3dcc
Elliott <elliottohj44@gmail.com>
2021-06-23 23:18:54 -0400

    fixed issue with button not focusing and enhanced focus outline, made it more visible

--- src/components/ContactForm/MyForm.tsx
+++ src/components/ContactForm/MyForm.tsx
@@ -22,15 +22,13 @@ const InputField = forwardRef((props, ref) => {
         Let's inline some validation and error information
         for our field
       */}
-      <div className="validation-message">
-        {isValidating ? (
-          <em>Validating...</em>
-        ) : isTouched && error ? (
-          <strong>{error}</strong>
-        ) : message ? (
-          <small>{message}</small>
-        ) : null}
-      </div>
+      {isValidating ? (
+        <em>Validating...</em>
+      ) : isTouched && error ? (
+        <strong>{error}</strong>
+      ) : message ? (
+        <small>{message}</small>
+      ) : null}
     </>
   )
 })
@@ -47,79 +45,85 @@ function MyForm() {
   )
   const {
     Form,
-    // values,
+    values,
     // pushFieldValue,
     // removeFieldValue,
+    // handleSubmit,
     meta: { isSubmitting, isSubmitted, canSubmit, error },
   } = useForm({
     defaultValues,
     validate: (values) => {
-      if (values.name === 'Elliott Jones') return 'No doppleganger spam!'
-      // if (!values.subject) return 'Subject is required'
+      if (values.name === 'Elliott Jones') return 'I do not accept spam from nominal doppelgängers.'
+      if (values.email === 'elliottohj44@gmail.com') return 'Please enter your own email address.'
       return false
     },
     onSubmit: async (values, instance) => {
       await new Promise((resolve) => setTimeout(resolve, 1000))
       console.log(values)
     },
-    debugForm: true,
+    // debugForm: true,
   })
 
   return (
     <section id="contact" className="contact">
-      <Form className="contact-form">
+      <Form 
+        className="contact-form" 
+        // onSubmit={(e:React.SyntheticEvent<unknown, Event>)=>e.preventDefault()} 
+      // handleSubmit={(e:React.SyntheticEvent<unknown, Event>) => e.preventDefault()}
+      >
         <div className="input-wrapper">
           <label htmlFor="name">Name</label>
           <InputField /* @ts-ignore */
             id="name"
             field="name"
-            validate={(value:string) => (!value ? 'Required' : false)}
-            placeHolder="Name" />
+            placeholder="Full Name" 
+            validate={(value:string) => (!value ? ' Required' : false)}
+            />
         </div>
         <div className="input-wrapper">
           <label htmlFor="email">Email</label>
           <InputField /* @ts-ignore */
             id="email"
             field="email"
+            placeholder="Email" 
             validate={async (value:string) => {
-              if (!value) return 'Email is required'
-              if (!validateEmail(value)) return 'Please enter a valid email addresss'
+              if (!value) return ' Required'
+              if (!validateEmail(value)) return ' Please enter a valid email addresss'
               console.log(`Checking email: ${value}...`)
               // We're going to mock that for now
               await new Promise((resolve) => setTimeout(resolve, 2000))
-              
-              return value === 'elliottohj44@gmail.com' ? 'Email is already being used' : false
+              return false
             }}
-            placeHolder="Email" />
+            />
         </div>
         <div className="input-wrapper">
           <label htmlFor="subject">Subject</label>
           <InputField /* @ts-ignore */
             id="subject"
             field="subject" 
-            validate={(value:string) => (!value ? 'Required' : false)} 
-            placeHolder="Subject" />
+            placeholder="Subject"
+            validate={(value:string) => (!value ? ' Required' : false)}
+            />
         </div>
         <div className="input-wrapper">
           <label htmlFor="email-message">Message</label>
           <InputField /* @ts-ignore */ 
             id="email-message"
             field="message"
-            placeHolder="Message" />
+            placeholder="Message"
+            validate={(value:string) => (!value ? ' Required' : false)}
+            />
         </div>
         {isSubmitted && <em>Thanks for submitting!</em>}
         {error && <strong>{error}</strong>}
-        {isSubmitting ? (
-          'Submitting...'
-          ) : (
-          <div className="input-wrapper">
-            <button className="submit-btn grow" type="submit" disabled={!canSubmit}>
-              Submit
-            </button>
-          </div>
-        )}
+        {isSubmitting && 'Submitting...'}
+        <div className="input-wrapper">
+          <button className="submit-btn grow" type="submit" disabled={!!isSubmitting || !canSubmit}>Send</button>
+        </div>
       </Form>
-      {/* <pre>{JSON.stringify(values, undefined, 2)}</pre> */}
+      <code>
+        <pre>{JSON.stringify(values, undefined, 2)}</pre>
+      </code>
     </section>
   )
 }
